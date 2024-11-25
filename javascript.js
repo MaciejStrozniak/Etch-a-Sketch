ccf6eb33ad97c84609cf876700c8f2db657b92d7
MaciejStrozniak <maciejstrozniak@gmail.com>
2024-11-25 13:26:37 +0100

    Adding first version of flex-grid

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const squaresContId = "squares-container";
+
+const bodyEl = document.querySelector("body");
+const squaresContainer = document.createElement("div");
+squaresContainer.id = squaresContId;
+bodyEl.appendChild(squaresContainer);
+
+for(i=0; i < 255; i++) {
+    const divEl = document.createElement("div");
+    divEl.classList.add("grid-div");
+    divEl.textContent = "It's just a drill";
+    squaresContainer.appendChild(divEl);
+};
