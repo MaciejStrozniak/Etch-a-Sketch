16c760358e553f6f4084ad0334a2ce9ae79e12c9
MaciejStrozniak <maciejstrozniak@gmail.com>
2024-11-26 13:03:35 +0100

    Moved small squares sizing to javascript.js. Created createSmallSquare() function and added functional button to the page via js.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,41 @@
+const log = console.log;
 const squaresContId = "squares-container";
 
 const bodyEl = document.querySelector("body");
 const squaresContainer = document.createElement("div");
 squaresContainer.id = squaresContId;
-bodyEl.appendChild(squaresContainer);
+const buttonToChangeGrid = document.createElement("button");
+buttonToChangeGrid.id = "Change-grid-btn";
+buttonToChangeGrid.textContent = "Change grid size";
+buttonToChangeGrid.style.margin = "10px 0px 0px 0px";
 
-for(i=0; i <= 255; i++) {
+bodyEl.appendChild(squaresContainer);
+bodyEl.appendChild(buttonToChangeGrid);
+
+const containerSize = 800;
+let gridSize = 16;
+let smallSquareSize = containerSize / gridSize;
+
+function createSmallSquare() {
     const divEl = document.createElement("div");
+    divEl.style.height = `${smallSquareSize.toString()}px`;
+    divEl.style.width = `${smallSquareSize.toString()}px`;
+    divEl.style.border = "solid 2px grey";
     divEl.classList.add("grid-div");
+
+    return divEl;
+}
+
+for(i=0; i < gridSize * gridSize; i++) {
+    const divEl = createSmallSquare();
     divEl.textContent = `${i}`;
     squaresContainer.appendChild(divEl);
 };
+
+buttonToChangeGrid.addEventListener("click", () => {
+    let newGridSize;
+    
+    if(buttonToChangeGrid.id === "Change-grid-btn")
+        newGridSize = prompt("Choose size between 1 and 100");
+});
+
