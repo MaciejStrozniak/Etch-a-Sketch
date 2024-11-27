d58ebd040bed103d666df5dac67206d68b960ecd
MaciejStrozniak <maciejstrozniak@gmail.com>
2024-11-27 13:31:11 +0100

    Version 1.0 of the project. Full basic functionality added.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,26 @@
 const log = console.log;
 const squaresContId = "squares-container";
+const buttonToChangeId = "Change-grid-btn";
+const buttonToResetId = "Reset-grid-btn";
 
 const bodyEl = document.querySelector("body");
+
 const squaresContainer = document.createElement("div");
 squaresContainer.id = squaresContId;
+
 const buttonToChangeGrid = document.createElement("button");
-buttonToChangeGrid.id = "Change-grid-btn";
+buttonToChangeGrid.id = buttonToChangeId;
 buttonToChangeGrid.textContent = "Change grid size";
 buttonToChangeGrid.style.margin = "10px 0px 0px 0px";
 
+const buttonToResetGrid = document.createElement("button");
+buttonToResetGrid.id = buttonToResetId;
+buttonToResetGrid.textContent = "Reset grid";
+buttonToResetGrid.style.margin = "10px 0px 0px 0px";
+
 bodyEl.appendChild(squaresContainer);
 bodyEl.appendChild(buttonToChangeGrid);
+bodyEl.appendChild(buttonToResetGrid);
 
 const containerSize = 800;
 let gridSize = 16;
@@ -21,6 +31,7 @@ function createSquare(gridSize) {
     const divEl = document.createElement("div");
     divEl.style.height = `${smallSquareSize.toString()}px`;
     divEl.style.width = `${smallSquareSize.toString()}px`;
+    divEl.style.backgroundColor = "rgb(100, 242, 127)";
     divEl.style.border = "solid 0.5px grey";
     divEl.classList.add("grid-div");
 
@@ -43,12 +54,16 @@ function createGrid(gridSize) {
     }
 };
 
-createGrid(gridSize);
+function resetGridColor() {
 
-buttonToChangeGrid.addEventListener("click", () => {
-    
-    if(buttonToChangeGrid.id === "Change-grid-btn") {
-    
+    const elToReset = document.querySelectorAll(".grid-div");
+
+    elToReset.forEach((el) => {
+        el.style.backgroundColor = "rgb(100, 242, 127)";
+    });
+}
+
+function userPrompt() {
         let loop = true;
 
         gridSize = prompt("Choose size of grid between 1 and 100");
@@ -57,13 +72,45 @@ buttonToChangeGrid.addEventListener("click", () => {
             if(gridSize == null || typeof(gridSize) !== "number" || isNaN(gridSize))
                 gridSize = prompt("Wrong value. Choose size between 1 and 100");
             
-            if(gridSize > 100 || gridSize <= 1)
+            if(gridSize > 100 || gridSize < 1)
                 gridSize = prompt("Wrong size of grid. Choose size between 1 and 100");
             else
                 loop = false;
         }
+}
 
-        removeSquares(gridSize);
-        createGrid(gridSize);
-    }
+function generateNewGrid() {
+    userPrompt();
+    removeSquares(gridSize);
+    createGrid(gridSize);
+}
+
+buttonToChangeGrid.addEventListener("click", () => {
+    
+    if(buttonToChangeGrid.id === "Change-grid-btn")
+       generateNewGrid();
 });
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "Enter")
+        buttonToChangeGrid.click();
+});
+
+buttonToResetGrid.addEventListener("click", () =>{
+    resetGridColor();
+});
+
+document.body.addEventListener("keydown", (e) => {
+    if(e.key === " ")
+        buttonToResetGrid.click();
+});
+
+document.body.addEventListener("mouseover", (e) => {
+    const target = e.target;
+
+    if(target.classList.contains("grid-div"))
+        target.style.backgroundColor = "black";        
+});
+
+// DEFAULT SETUP
+createGrid(gridSize);
